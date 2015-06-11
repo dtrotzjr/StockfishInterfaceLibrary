44b7975ec65bd4f26901851106b348661d0079bd
David Trotz <david.trotz@gmail.com>
2015-06-10 20:02:03 -0700

    Initial Commit

---
+++ StockfishInterfaceLibrary/StockfishInterfaceLibrary.h
@@ -0,0 +1,13 @@
+//
+//  StockfishInterfaceLibrary.h
+//  StockfishInterfaceLibrary
+//
+//  Created by David Trotz on 6/10/15.
+//  Copyright (c) 2015 AppPoetry LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StockfishInterfaceLibrary : NSObject
+
+@end
---
+++ StockfishInterfaceLibrary/StockfishInterfaceLibrary.m
@@ -0,0 +1,13 @@
+//
+//  StockfishInterfaceLibrary.m
+//  StockfishInterfaceLibrary
+//
+//  Created by David Trotz on 6/10/15.
+//  Copyright (c) 2015 AppPoetry LLC. All rights reserved.
+//
+
+#import "StockfishInterfaceLibrary.h"
+
+@implementation StockfishInterfaceLibrary
+
+@end
